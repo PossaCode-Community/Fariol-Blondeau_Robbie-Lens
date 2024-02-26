980d56913bfbaec38c857055a783873a01179f4e
brx-nbif <blondeau.nbif@mail.ru>
2024-02-26 20:12:11 +0100

    UPDATE]: Updating the main.ts file by commenting the formData

--- src/script/main.ts
+++ src/script/main.ts
@@ -66,15 +66,10 @@ async function handleSubmit(event: Event) {
   const form = event.target as HTMLFormElement | null;
 
   if (form) {
-    const formData = new FormData(form);
-
-    const name = formData.get("name");
-    const email = formData.get("email");
-    const message = formData.get("message");
+    // const formData = new FormData(form);
 
     await wait(3000);
     // TODO: Send data to the database
-    // console.log(name, email, message);
 
     form.reset();
   }
